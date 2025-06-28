bc989ad1a4c19a4e02435e223b7afa39104daf8e
lileskimo <kaustubh.salodkar13@gmail.com>
2025-06-29 00:50:53 +0530

    trying to deploy again

--- server/index.js
+++ server/index.js
@@ -8,7 +8,10 @@ import { fileURLToPath } from 'url'
 dotenv.config()
 
 const app = express()
-app.use(cors())
+app.use(cors({
+  origin: 'https://statify.up.railway.app',
+  credentials: true
+}))
 app.use(express.json())
 
 const spotifyApi = new SpotifyWebApi({
