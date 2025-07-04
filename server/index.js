46fda17c05405f7e50b61031cb9294d66852b425
lileskimo <kaustubh.salodkar13@gmail.com>
2025-07-04 11:43:38 +0530

    tinkering with port

--- server/index.js
+++ server/index.js
@@ -156,5 +156,5 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = 8080;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
